9186d1705bb855459c2a4cd2f9c774b06e00943c
Eric Cranston <cracell@gmail.com>
2018-06-22 11:11:21 -0700

    Test fix

--- spec/lib/synchronized_model/receive_mixin_spec.rb
+++ spec/lib/synchronized_model/receive_mixin_spec.rb
@@ -1,22 +1,25 @@
 # frozen_string_literal: true
 require 'spec_helper'
+require 'pry-byebug'
 
 RSpec.describe SynchronizedModel::ReceiveMixin do
-  class TestClass
+  class TestModelReceive
     include SynchronizedModel::ReceiveMixin
     synchronized_model_receive
   end
 
   describe '.synchronized_model_receive' do
     subject do
-      TestClass.new
+      TestModel.new
     end
 
     it 'returns attributes hash' do
       subject
 
-      expect(SynchronizedModel::ModelMessage.resource_classes)
-        .to eq(test_class: TestClass)
+      expect(
+        SynchronizedModel::ModelMessage.resource_classes[:test_model_receive]
+      )
+        .to eq(TestModelReceive)
     end
   end
 end
