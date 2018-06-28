aaf6322e0ade5e4f8792d2a39f6ad27e65c13b3c
Eric Cranston <cracell@gmail.com>
2018-06-28 12:15:53 -0700

    Refactor

--- lib/synchronized_model/support.rb
+++ lib/synchronized_model/support.rb
@@ -3,7 +3,9 @@ module SynchronizedModel
   module Support
     # Taken from ActiveSupport:Inflector.underscore
     def underscore(camel_cased_word)
-      return camel_cased_word unless matches_wrapper(camel_cased_word)
+      unless matches(/[A-Z-]|::/, camel_cased_word)
+        return camel_cased_word
+      end
       word = camel_cased_word.to_s.gsub('::', '/')
       word.gsub!(/([A-Z\d]+)([A-Z][a-z])/, '\1_\2')
       word.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
@@ -16,16 +18,12 @@ module SynchronizedModel
 
     # `match?` was added in Ruby 2.4 this allows us to be backwards
     # compatible with older Ruby versions
-    def matches(word)
+    def matches(regexp, word)
       if regexp.respond_to?(:match?)
         regexp.match?(word)
       else
         regexp.match(word)
       end
-    end
-
-    def camel_case_regexp
-      /[A-Z-]|::/
     end
   end
 end
