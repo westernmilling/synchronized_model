e6c5fda53aa2458226a7e153a7a03d990d3accbc
Eric Cranston <cracell@gmail.com>
2018-06-28 12:12:11 -0700

    Fallback for matches? to support older version of ruby
    Gem is using .matches? method which was added in Ruby 2.4
    It behaves the same as .matches however it's far faster.
    
    Item master however is not yet running on Ruby 2.4 so adding
    fallback for when matches? isn't available to support it.
    
    needed-by westernmilling/entity_master_app#229

--- lib/synchronized_model/support.rb
+++ lib/synchronized_model/support.rb
@@ -3,13 +3,29 @@ module SynchronizedModel
   module Support
     # Taken from ActiveSupport:Inflector.underscore
     def underscore(camel_cased_word)
-      return camel_cased_word unless /[A-Z-]|::/.match?(camel_cased_word)
+      return camel_cased_word unless matches_wrapper(camel_cased_word)
       word = camel_cased_word.to_s.gsub('::', '/')
       word.gsub!(/([A-Z\d]+)([A-Z][a-z])/, '\1_\2')
       word.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
       word.tr!('-', '_')
       word.downcase!
       word
+    end
+
+    private
+
+    # `match?` was added in Ruby 2.4 this allows us to be backwards
+    # compatible with older Ruby versions
+    def matches(word)
+      if regexp.respond_to?(:match?)
+        regexp.match?(word)
+      else
+        regexp.match(word)
+      end
+    end
+
+    def camel_case_regexp
+      /[A-Z-]|::/
     end
   end
 end
