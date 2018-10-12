d5be7382041e00b03d2da92f49445de0cdbd0295
Jose C Fernandez <jfernandez@westernmilling.com>
2018-10-12 12:15:51 -0400

    Add test coverage for Sequel.

--- spec/lib/synchronized_model/message_receive_spec.rb
+++ spec/lib/synchronized_model/message_receive_spec.rb
@@ -41,6 +41,15 @@ RSpec.describe SynchronizedModel::MessageReceive do
       model
     end
 
+    let(:mock_model_sequel) do
+      model = TestModel.new
+      allow(model).to receive(:save!)
+      allow(model).to receive(:column_change).and_return(
+        [updated_at_was, updated_at]
+      )
+      model
+    end
+
     let(:logger_mock) do
       logger = double
       allow(logger).to receive(:info)
@@ -57,9 +66,6 @@ RSpec.describe SynchronizedModel::MessageReceive do
             'test_model': TestModel
           }
       end
-      allow(mock_message_model)
-        .to receive(:model)
-        .and_return(mock_model)
       allow(SynchronizedModel::ModelMessage)
         .to receive(:new)
         .and_return(mock_message_model)
@@ -67,65 +73,89 @@ RSpec.describe SynchronizedModel::MessageReceive do
 
     subject { SynchronizedModel::MessageReceive.new(message).call }
 
-    context 'it\'s a chronological_update' do
-      let(:updated_at_was) { Time.now.to_i }
-      let(:updated_at) { Time.now.to_i + 100 }
-
-      it 'calls save!' do
-        subject
-
-        expect(mock_model).to have_received(:save!)
-      end
-    end
-
-    context 'the message is older than the model' do
-      let(:updated_at_was) { Time.now.to_i + 10_000 }
-      let(:updated_at) { Time.now.to_i }
-
-      it 'does not call save!' do
-        subject
-
-        expect(mock_model).to_not have_received(:save!)
-      end
-    end
-
-    context 'the message is the same age as the model' do
-      let(:updated_at_was) { Time.now.to_i }
-      let(:updated_at) { Time.now.to_i }
-
-      it 'does not call save!' do
-        subject
-
-        expect(mock_model).to_not have_received(:save!)
-      end
-    end
-
-    context "the model isn't configured" do
-      let(:updated_at_was) { nil }
-      let(:updated_at) { nil }
-      let(:expected_log_message) do
-        "Skipped Message ID: #{message[:id]}. " \
-        "No #{message[:resource]} model configured with SynchronizedModel"
-      end
-      let(:logger_spy) { spy }
-      let(:mock_model) { nil }
-
+    context 'with a Sequel model' do
       before do
-        allow(SynchronizedModel)
-          .to receive(:logger)
-          .and_return(logger_spy)
-        allow(logger_spy)
-          .to receive(:info)
+        allow(mock_message_model)
+          .to receive(:model).and_return(mock_model_sequel)
       end
 
-      it 'logs that the model was not configured' do
-        subject
+      context 'it\'s a chronological_update' do
+        let(:updated_at_was) { Time.now.to_i }
+        let(:updated_at) { Time.now.to_i + 100 }
 
-        expect(SynchronizedModel)
-          .to have_received(:logger)
-        expect(logger_spy)
-          .to have_received(:info)
-          .with(expected_log_message)
+        it 'calls save!' do
+          subject
+
+          expect(mock_model_sequel).to have_received(:save!)
+        end
+      end
+    end
+
+    context 'with an ActiveRecord model' do
+      before do
+        allow(mock_message_model).to receive(:model).and_return(mock_model)
+      end
+
+      context 'it\'s a chronological_update' do
+        let(:updated_at_was) { Time.now.to_i }
+        let(:updated_at) { Time.now.to_i + 100 }
+
+        it 'calls save!' do
+          subject
+
+          expect(mock_model).to have_received(:save!)
+        end
+      end
+
+      context 'the message is older than the model' do
+        let(:updated_at_was) { Time.now.to_i + 10_000 }
+        let(:updated_at) { Time.now.to_i }
+
+        it 'does not call save!' do
+          subject
+
+          expect(mock_model).to_not have_received(:save!)
+        end
+      end
+
+      context 'the message is the same age as the model' do
+        let(:updated_at_was) { Time.now.to_i }
+        let(:updated_at) { Time.now.to_i }
+
+        it 'does not call save!' do
+          subject
+
+          expect(mock_model).to_not have_received(:save!)
+        end
+      end
+
+      context "the model isn't configured" do
+        let(:updated_at_was) { nil }
+        let(:updated_at) { nil }
+        let(:expected_log_message) do
+          "Skipped Message ID: #{message[:id]}. " \
+          "No #{message[:resource]} model configured with SynchronizedModel"
+        end
+        let(:logger_spy) { spy }
+        let(:mock_model) { nil }
+
+        before do
+          allow(SynchronizedModel)
+            .to receive(:logger)
+            .and_return(logger_spy)
+          allow(logger_spy)
+            .to receive(:info)
+        end
+
+        it 'logs that the model was not configured' do
+          subject
+
+          expect(SynchronizedModel)
+            .to have_received(:logger)
+          expect(logger_spy)
+            .to have_received(:info)
+            .with(expected_log_message)
+        end
       end
     end
   end
